48388b3254a206a931c93fd919e2a1a57d6e45a4
Abudi Mutamba <abudimutamba@Abudis-MacBook-Pro.local>
2021-08-23 12:50:25 +0300

    setTimeout with params

--- Session10/timers.js
+++ Session10/timers.js
@@ -8,3 +8,9 @@ setTimeout (() => {
   //runs after 50 milliseconds
   console.log("Ran after 2 seconds")
 },500)
+
+//setTimeout with a params
+let greeting = (name, msg) => console.log(`${msg} ${name}`)
+
+setTimeout(greeting, 1000, 'David', 'Hola')
+setTimeout(greeting, 500, 'Abudi', 'Agandi')
