91f581d34a157cead5abf92fdba655cf12162825
Abudi Mutamba <abudimutamba@Abudis-MacBook-Pro.local>
2021-08-24 11:39:32 +0300

    Zero delay

---
+++ Session10/timers2.js
@@ -0,0 +1,14 @@
+//Zero delay
+setTimeout(() => {  
+        console.log('after')
+},0)
+console.log('before')
+
+setTimeout(() => { 
+    console.log('The first')
+},0)
+setTimeout(() => { 
+    console.log('The second')
+},0)
+
+
