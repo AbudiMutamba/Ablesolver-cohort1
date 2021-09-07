af583f6346c99022c431d43025f621696a3e6670
Abudi Mutamba <abudimutamba@Abudis-MacBook-Pro.local>
2021-09-07 10:37:43 +0300

    Request-handling

---
+++ Session10/request-handling.js
@@ -0,0 +1,16 @@
+const https = require('https')
+const options = {
+    hostname: 'example.com',
+    port:443,
+    path:'/todos',
+    method: 'GET'
+}
+
+const request = https.request(options, (response) => {
+    console.log(`statuscode ${response.statuscode}`)
+
+    response.on('data', returnedData => process.stdout.write(returnedData))
+})
+
+request.on('error', (error) => console.error(error))
+request.end()
