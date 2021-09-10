80090995d1221cfa44de76092f18bfbd544155ba
Abudi Mutamba <abudimutamba@Abudis-MacBook-Pro.local>
2021-09-10 10:45:42 +0300

    file path

--- Session10/file-path.js
+++ Session10/file-path.js
@@ -1,1 +1,9 @@
-const path =
+const path = require('path')
+
+const notes = '/users/joe/notes.txt'
+
+console.log(path.dirname(notes)) // /use
+console.log(path.basename(notes))
+console.log(path.extname(notes))
+console.log(path.basename(notes, path.extname(notes)))
+
