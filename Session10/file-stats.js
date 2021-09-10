b8ff14ecfc93b870588674b99a02b9dab57bc3c4
Abudi Mutamba <abudimutamba@Abudis-MacBook-Pro.local>
2021-09-10 10:40:55 +0300

    Other methods of stats

--- Session10/file-stats.js
+++ Session10/file-stats.js
@@ -1,14 +1,16 @@
-// const fs = require('fs')
-// fs.stat('test.text', (err, stats) => {
-//     if(err) {
-//         console.error(err)
-//         return
-//     }
+/*const fs = require('fs')
+fs.stat('test.text', (err, stats) => {
+    if(err) {
+        console.error(err)
+        return
+    }
 
-//     console.log(stats)
-//     //process.stdout.write(stats)
-// })
+    console.log(stats)
+    //process.stdout.write(stats)
+})
+*/
 
+/*
 const fs = require('fs')
 try {
     const stats = fs.statSync('test.text')
@@ -18,3 +20,16 @@ try {
 }
 
 console.log('After the stats')
+*/
+
+const fs = require('fs')
+
+try {
+    const stats = fs.statSync('test.text')
+    console.log(`Is it a file? ${stats.isFile()}`)
+    console.log(`Is it a directory? ${stats.isDirectory()}`)
+    console.log(`File size in bytes: ${stats.size}`)
+    console.log(`Is it a symbolic link? ${stats.isSymbolicLink()}`)
+} catch(err) {
+    console.error(err)
+}
