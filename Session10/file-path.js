17babd287d0f394c7608cb4e1e981b84a06a1fdf
Abudi Mutamba <abudimutamba@Abudis-MacBook-Pro.local>
2021-09-17 09:44:26 +0300

    Setting up branch

--- Session10/file-path.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-const notes = '/users/joe/notes.txt'
-
-console.log(path.dirname(notes)) // /use
-console.log(path.basename(notes))
-console.log(path.extname(notes))
-console.log(path.basename(notes, path.extname(notes)))
-d
